f2c8f1ee06557400d9aac37ea2a81b862c3335d3
MrAaronCasanova <1boris231@gmail.com>
2018-04-01 23:00:33 -0700

    going to try to adding modulus logic to gallery

--- casy-collection/src/components/ImageComponents/ImageEffects/UnderlineTextImage/UnderlineTextImage.js
+++ casy-collection/src/components/ImageComponents/ImageEffects/UnderlineTextImage/UnderlineTextImage.js
@@ -1,15 +1,13 @@
 import React from 'react';
 import styles from './UnderlineTextImage.css';
+import HalvingStyles from '../../ImageGalleries/HalvingGallery/HalvingGallery.css';
 
-const UnderlineTextImage = () => {
+const UnderlineTextImage = props => {
   return (
-    <div className={styles.imageGalleryPicture}>
-      <h2 className={styles.imageGalleryTitle}>Images</h2>
-      <a href="components/images/images-index.html">
-        <img
-          src="https://images.pexels.com/photos/327803/pexels-photo-327803.jpeg?auto=compress&cs=tinysrgb&dpr=2&h=650&w=940"
-          alt="Section Button"
-        />
+    <div className={`${styles.imageGalleryPicture} ${props.large}`}>
+      <h2 className={styles.imageGalleryTitle}>{props.category.title}</h2>
+      <a href="/">
+        <img src={props.category.src} alt={props.category.alt} />
       </a>
     </div>
   );
--- casy-collection/src/components/ImageComponents/ImageGalleries/HalvingGallery/HalvingGallery.js
+++ casy-collection/src/components/ImageComponents/ImageGalleries/HalvingGallery/HalvingGallery.js
@@ -1,10 +1,93 @@
 import React from 'react';
 import UnderlineTextImage from '../../ImageEffects/UnderlineTextImage/UnderlineTextImage';
+import styles from './HalvingGallery.css';
+
+const categories = [
+  {
+    title: 'Navbars',
+    src:
+      'https://images.pexels.com/photos/697662/pexels-photo-697662.jpeg?auto=compress&cs=tinysrgb&dpr=2&h=750&w=1260',
+    alt: 'Navbars Section'
+  },
+  {
+    title: 'Images',
+    src:
+      'https://images.pexels.com/photos/327803/pexels-photo-327803.jpeg?auto=compress&cs=tinysrgb&dpr=2&h=650&w=940',
+    alt: 'Images Section'
+  },
+  {
+    title: 'Buttons',
+    src:
+      'https://images.pexels.com/photos/243059/pexels-photo-243059.jpeg?auto=compress&cs=tinysrgb&dpr=2&h=650&w=940',
+    alt: 'Buttons Section'
+  },
+  {
+    title: 'Placeholder1',
+    src: 'https://source.unsplash.com/random/800x600',
+    alt: 'Buttons Section'
+  },
+  {
+    title: 'Placeholder2',
+    src: 'https://source.unsplash.com/random/800x601',
+    alt: 'Buttons Section'
+  },
+  {
+    title: 'Placeholder3',
+    src: 'https://source.unsplash.com/random/800x602',
+    alt: 'Buttons Section'
+  },
+  {
+    title: 'Placeholder4',
+    src: 'https://source.unsplash.com/random/800x603',
+    alt: 'Buttons Section'
+  },
+  {
+    title: 'Placeholder5',
+    src: 'https://source.unsplash.com/random/800x604',
+    alt: 'Buttons Section'
+  },
+  {
+    title: 'Placeholder6',
+    src: 'https://source.unsplash.com/random/800x605',
+    alt: 'Buttons Section'
+  },
+  {
+    title: 'Placeholder7',
+    src: 'https://source.unsplash.com/random/800x606',
+    alt: 'Buttons Section'
+  }
+];
 
 const HalvingGallery = () => {
+  let count = 0;
   return (
-    <div>
-      <UnderlineTextImage />
+    <div className={styles.HalvingGallery}>
+      <div className={styles.imageContainer}>
+        {categories.map(category => {
+          count++;
+          console.log(count);
+          if (count < 4) {
+            return (
+              <UnderlineTextImage
+                large={styles.largeThree}
+                key={category.title}
+                category={category}
+              />
+            );
+          } else if (count < 6) {
+            if (count === 5) {
+              count = 0;
+            }
+            return (
+              <UnderlineTextImage
+                large={styles.largeTwo}
+                key={category.title}
+                category={category}
+              />
+            );
+          }
+        })}
+      </div>
     </div>
   );
 };
