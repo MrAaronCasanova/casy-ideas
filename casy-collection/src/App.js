f99c536d6a9f8e354ee67b0bdd562c467d18302b
MrAaronCasanova <1boris231@gmail.com>
2018-04-12 20:51:18 -0700

    removed old nav code from app.js

--- casy-collection/src/App.js
+++ casy-collection/src/App.js
@@ -1,13 +1,12 @@
 import React, { Component } from 'react';
-import { BrowserRouter, Route, Link, Switch } from 'react-router-dom';
+import { BrowserRouter, Route, Switch } from 'react-router-dom';
 // BroswerRouter is an object that interacts with the url history
 // Route executes whatever the BrowserRouter passes
 // ------ Components ------ //
-import SingleTitleNav from './components/NavigationComponents/Navbars/SingleTitleNav/SingleTitleNav';
+import Profiles from './components/Routes/Profiles/Profiles';
 // ------------------------ //
 // ------ Routs ------- //
 import Home from './components/Routes/Home';
-import Profiles from './components/Routes/Profiles/Profiles';
 import Buttons from './components/Routes/Buttons/Buttons';
 import Cards from './components/Routes/Cards/Cards';
 import Navigation from './components/Routes/Navigation/Navigation';
@@ -22,86 +21,13 @@ import { RoutesData } from './DumbyData';
 import { CompanyInfo } from './DumbyData';
 
 class App extends Component {
-  state = {
-    open: false,
-    nav: `${styles.flexLinks} ${styles.close}`,
-    placeHolderToggler: '☰'
-  };
-
-  closeMenu = () => {
-    console.log(this.state.open);
-    const update = { ...this.state };
-    if (update.open) {
-      update.nav = `${styles.flexLinks} ${styles.close}`;
-      update.placeHolderToggler = '☰';
-      update.open = false;
-    } else {
-      update.nav = styles.flexLinks;
-      update.placeHolderToggler = '✕';
-      update.open = true;
-    }
-    this.setState({
-      ...update
-    });
-  };
-
   render() {
     return (
       <BrowserRouter>
         {/* ------ Page Wrapper ------ */}
         <div className={styles.pageWrapper}>
           {/* ------ Main Nav ------ */}
-          {/* <Link className={styles.HomeLink} to="/">
-            <SingleTitleNav />
-            <div className={styles.placeHolderToggler} onClick={this.closeMenu}>
-              {this.state.placeHolderToggler}
-            </div>
-          </Link> */}
-          {/* ;alksjd;lkfja;lksjd;fklja */}
           <Profiles logo={CompanyInfo.name} routes={RoutesData} />
-          {/* klajs;dlkjf;akjsd;lkfj;ak */}
-
-          {/* ------ Main Nav Links ------ */}
-          <ul className={this.state.nav}>
-            <Link onClick={this.closeMenu} className={styles.flexLink} to="/">
-              Home
-            </Link>
-            <Link
-              onClick={this.closeMenu}
-              className={styles.flexLink}
-              to="/profiles"
-            >
-              Profiles
-            </Link>
-            <Link
-              onClick={this.closeMenu}
-              className={styles.flexLink}
-              to="/buttons"
-            >
-              Buttons
-            </Link>
-            <Link
-              onClick={this.closeMenu}
-              className={styles.flexLink}
-              to="/cards"
-            >
-              Cards
-            </Link>
-            <Link
-              onClick={this.closeMenu}
-              className={styles.flexLink}
-              to="/navigation"
-            >
-              Navigation
-            </Link>
-            <Link
-              onClick={this.closeMenu}
-              className={styles.flexLink}
-              to="/landing-pages"
-            >
-              Landing Pages
-            </Link>
-          </ul>
 
           {/* ------ Routes ------ */}
           <Switch>
@@ -125,3 +51,8 @@ export default App;
 // on video 038 CSSTransition Basics
 // watch vid 52 for CSSTransition in action (folder 5)
 // move on to folder 6
+
+// ------ Todo ------ //
+// if statement for nav links
+// refactor/rename entire nav
+// change Profiles to actual Nav Component
