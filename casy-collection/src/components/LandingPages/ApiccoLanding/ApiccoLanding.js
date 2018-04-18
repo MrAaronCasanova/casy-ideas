ea1f894e1b0cb900c36c284fc8662fd4213ffad7
MrAaronCasanova <1boris231@gmail.com>
2018-04-18 07:17:48 -0700

    apicco landing pages home stretch

--- casy-collection/src/components/LandingPages/ApiccoLanding/ApiccoLanding.js
+++ casy-collection/src/components/LandingPages/ApiccoLanding/ApiccoLanding.js
@@ -14,7 +14,11 @@ const ApiccoLanding = () => {
         <p className={styles.link}>View Case Study</p>
       </div>
       <div className={styles.imgContainer}>
-        <img src="https://source.unsplash.com/random" alt="" />
+        <img
+          className={styles.image}
+          src="https://images.pexels.com/photos/425160/pexels-photo-425160.jpeg?auto=compress&cs=tinysrgb&dpr=2&h=750&w=1260"
+          alt=""
+        />
         <div className={styles.shape} />
       </div>
     </div>
