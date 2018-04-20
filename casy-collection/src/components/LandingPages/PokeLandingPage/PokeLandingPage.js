deb731793cf5511910f997f2d40926112d7184d7
MrAaronCasanova <1boris231@gmail.com>
2018-04-20 08:13:22 -0700

    gonna make a new branch and try something else

--- casy-collection/src/components/LandingPages/PokeLandingPage/PokeLandingPage.js
+++ casy-collection/src/components/LandingPages/PokeLandingPage/PokeLandingPage.js
@@ -12,17 +12,22 @@ const PokeLandingPage = () => {
           <li>Contact</li>
         </ul>
         <h2 className={styles.mainHeading}>Poke</h2>
+        <img
+          className={styles.circle}
+          src="http://www.onlygfx.com/wp-content/uploads/2017/07/circle-brush-stroke-12.png"
+          alt="circle"
+        />
+        <img
+          className={`${styles.img} ${styles.man}`}
+          src="https://images.pexels.com/photos/156731/pexels-photo-156731.jpeg?auto=compress&cs=tinysrgb&dpr=2&h=750&w=1260"
+          alt="record player"
+        />
+        <img
+          className={`${styles.img} ${styles.piano}`}
+          src="https://images.pexels.com/photos/159420/piano-instrument-music-keys-159420.jpeg?auto=compress&cs=tinysrgb&dpr=2&h=750&w=1260"
+          alt="piano"
+        />
       </div>
-      <img
-        className={`${styles.img} ${styles.record}`}
-        src="https://images.pexels.com/photos/145707/pexels-photo-145707.jpeg?auto=compress&cs=tinysrgb&dpr=2&h=750&w=1260"
-        alt="record player"
-      />
-      <img
-        className={`${styles.img} ${styles.piano}`}
-        src="https://images.pexels.com/photos/159420/piano-instrument-music-keys-159420.jpeg?auto=compress&cs=tinysrgb&dpr=2&h=750&w=1260"
-        alt="piano"
-      />
     </div>
   );
 };
