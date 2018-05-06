15fe43003452dd13bd113b2be054a043f705bba1
casyjs <1boris231@gmail.com>
2018-05-06 10:23:25 -0700

    bs commit / boxy

--- casy-collection/src/components/LandingPages/BoxyLanding/BoxyLanding.js
+++ casy-collection/src/components/LandingPages/BoxyLanding/BoxyLanding.js
@@ -2,9 +2,9 @@ import React from 'react';
 import styled from 'styled-components';
 
 const Wrapper = styled.div`
+  @import url('https://fonts.googleapis.com/css?family=Libre+Baskerville');
   height: 100vh;
   background: #fff;
-  padding: 150px;
   display: flex;
   justify-content: center;
   align-items: center;
@@ -12,24 +12,22 @@ const Wrapper = styled.div`
 
 const OutlineBox = styled.div`
   border: 2px solid #d4d4d4;
-  width: 100%;
-  height: 80%;
+  width: 75%;
+  height: 60%;
   display: flex;
 `;
 
 const ImagesWrapper = styled.div`
   flex: 1 1 50%;
-  background: #c9c9c9;
 `;
 
 const LargeImgWrapper = styled.div`
-  /* ---------For Editing Layout---------- */
-  border: 2px solid red;
-  /* ------------------------------------- */
-  width: 26vw;
-  height: 26vw;
-  transform: rotate(-45deg) translate(-20px, 15px);
+  border: 30px solid white;
+  width: 30vw;
+  height: 30vw;
+  transform: rotate(-45deg) translate(-20px, 35px);
   overflow: hidden;
+  position: relative;
 
   img {
     transform: rotate(45deg) translate(-25%, -10%);
@@ -39,9 +37,61 @@ const LargeImgWrapper = styled.div`
   }
 `;
 
+const SmallImgWrapper = styled.div`
+  border-top: 20px solid white;
+  border-left: 20px solid white;
+  width: 15vw;
+  height: 15vw;
+  overflow: hidden;
+  position: absolute;
+  top: 50%;
+  left: 50%;
+
+  img {
+    transform: rotate(45deg) translate(-25%, -7%);
+    width: 150%;
+    height: 150%;
+    object-fit: cover;
+  }
+`;
+
 const ContentWrapper = styled.div`
   flex: 1 1 50%;
-  background: #f7f7f7;
+  text-align: right;
+  display: flex;
+  flex-direction: column;
+  justify-content: center;
+
+  div {
+    transform: translate(14%, -5%);
+    background: #ffffff;
+    padding: 20px;
+    display: flex;
+    flex-direction: column;
+    justify-content: center;
+    align-items: flex-end;
+  }
+
+  p {
+    font-weight: bold;
+    margin-bottom: 40px;
+    font-size: 22px;
+  }
+
+  h2 {
+    margin-bottom: 50px;
+    font-family: 'Libre Baskerville', serif;
+    font-size: 100px;
+    line-height: 0.9;
+    font-weight: 400;
+  }
+
+  h6 {
+    font-size: 16px;
+    font-weight: bold;
+    width: 350px;
+    color: #888888;
+  }
 `;
 
 const BoxyLanding = () => {
@@ -52,11 +102,31 @@ const BoxyLanding = () => {
           <LargeImgWrapper>
             <img
               src="https://images.unsplash.com/photo-1517423568366-8b83523034fd?ixlib=rb-0.3.5&ixid=eyJhcHBfaWQiOjEyMDd9&s=92b82a18bf4bfbdfe1bd7eed8cd4ba49&auto=format&fit=crop&w=675&q=80"
-              alt="pug"
+              alt="dog"
             />
+            <SmallImgWrapper>
+              <img
+                src="https://images.unsplash.com/photo-1517213849290-bbbfffdc6da3?ixlib=rb-0.3.5&ixid=eyJhcHBfaWQiOjEyMDd9&s=943dfad4aa75252116f83032204a5608&auto=format&fit=crop&w=800&q=80"
+                alt="cat"
+              />
+            </SmallImgWrapper>
           </LargeImgWrapper>
         </ImagesWrapper>
-        <ContentWrapper>hello</ContentWrapper>
+        <ContentWrapper>
+          <div>
+            <p>
+              Friday 20 April<br />AM
+            </p>
+            <h2>
+              To Build<br />Relationships
+            </h2>
+            <h6>
+              Lorem ipsum dolor sit amet consectetur adipisicing elit. Ipsam
+              voluptatibus ut eveniet soluta ipsum quam enim libero obcaecati
+              cum architecto nisi
+            </h6>
+          </div>
+        </ContentWrapper>
       </OutlineBox>
     </Wrapper>
   );
