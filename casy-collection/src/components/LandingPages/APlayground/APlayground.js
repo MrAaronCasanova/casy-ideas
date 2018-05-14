ac9a10a5dcdc4a3fb4f8e4e073676cdcb0f2f15e
casyjs <1boris231@gmail.com>
2018-05-13 23:40:17 -0700

    figure out how to fr like growing behaviorwith minmax on grid template rows

--- casy-collection/src/components/LandingPages/APlayground/APlayground.js
+++ casy-collection/src/components/LandingPages/APlayground/APlayground.js
@@ -2,53 +2,88 @@ import React, { Component } from 'react';
 import styled from 'styled-components';
 
 const Wrapper = styled.div`
-  @import url('https://fonts.googleapis.com/css?family=Poppins');
+  /* ---------For Editing Layout---------- */
+  border: 2px solid red;
+  /* ------------------------------------- */
+  @import url('https://fonts.googleapis.com/css?family=Poppins:400,700');
+  /* @import url('https://fonts.googleapis.com/css?family=Poppins'); */
+
   @import url('https://fonts.googleapis.com/css?family=Josefin+Sans');
   /* padding: 40px 0; */
   /* background: #efe9ef;
   color: slategray; */
-  /* min-height: 100vh; */
+  min-height: 100vh;
   display: grid;
 
   @media (max-width: 750px) {
     grid-template-columns:
       [LeftGutter-start]
-      minmax(20px, 1fr)
+      /* 20px */
+      minmax(20px, 2fr)
       [LeftGutter-end Img2-start]
-      2fr
+      minmax(20px, 2fr)
+      /* minmax(20px, 2fr) */
+      /* 2fr */
       [Img1-start]
-      7fr
+      minmax(90px, 13fr)
+      /* minmax(180px, 7fr) */
+      /* 7fr */
       [Img1-end]
-      5fr
+      minmax(60px, 8fr)
+      /* minmax(20px, 5fr) */
+      /* 5fr */
       [Img3-start]
-      9fr
+      minmax(110px, 12fr)
+      /* minmax(20px, 9fr) */
+      /* 9fr */
       [Img2-end Img3-end RightGutter-start]
-      minmax(20px, 1fr)
+      /* 20px */
+      minmax(20px, 2fr)
       [RightGutter-end];
   }
   grid-template-rows:
     [TopGutter-start]
-    minmax(50px, 1fr)
+    20px
+    /* minmax(50px, 1fr) */
     [TopGutter-end Logo-start]
-    1fr
+    minmax(40px, 0)
+    /* 15vh */
+    /* 2fr */
     [Logo-end Nav-start]
-    1fr
+    minmax(55px, 0)
+    /* 20vh */
+    /* 4fr */
     [Nav-end Img1-start MainHeading-start]
-    1fr
+    minmax(80px, 0)
+    /* 25vh */
+    /* 6fr */
     [MainHeading-end Img2-start]
-    1fr
+    minmax(30px, 0)
+    /* 10vh */
+    /* 2fr */
     [Img1-end]
-    1fr
+    minmax(80px, 0)
+    /* 20vh */
+    /* 5fr */
     [Img3-start]
-    1fr
+    minmax(50px, 0)
+    /* 18vh */
+    /* 3fr */
     [Img2-end]
-    1fr
+    minmax(80px, 0)
+    /* 38vh */
+    /* 7fr */
     [Img3-end Email-start]
-    1fr
+    minmax(50px, 0)
+    /* 20vh */
+    /* 3fr */
     [Email-end Button-start]
-    1fr
+    minmax(50px, 0)
+    /* 20vh */
+    /* 3fr */
     [Button-end BottomGutter-start]
-    minmax(50px, 1fr)
+    20px
+    /* minmax(50px, 1fr) */
     [BottomGutter-end];
 `;
 
@@ -57,12 +92,22 @@ const Logo = styled.h2`
   border: 2px solid #48628c;
   /* ------------------------------------- */
   font-family: 'Poppins', sans-serif;
+  font-weight: bold;
 
   @media (max-width: 750px) {
     grid-column: LeftGutter-end / RightGutter-start;
     grid-row: Logo-start / Logo-end;
-    font-size: calc(
+    /* align-self: center; */
+    /* justify-self: center; */
+    display: flex;
+    justify-content: center;
+    align-items: center;
+
+    /* font-size: calc(
       45px + (60 - 45) * (${props => props.scale} - 320px) / (750 - 320)
+    ); */
+    font-size: calc(
+      23px + (60 - 23) * (${props => props.scale} - 320px) / (750 - 320)
     );
   }
 `;
@@ -72,17 +117,19 @@ const Nav = styled.ul`
   border: 2px solid #488c7e;
   /* ------------------------------------- */
   list-style: none;
-  /* display: flex;
-  justify-content: center; */
-  /* margin-bottom: 30px; */
 
   li {
-    /* padding: 5px; */
+    font-family: 'Poppins', sans-serif;
+    font-weight: bold;
+    padding: 10px;
   }
 
   @media (max-width: 750px) {
     grid-column: LeftGutter-end / RightGutter-start;
     grid-row: Nav-start / Nav-end;
+    display: flex;
+    justify-content: center;
+
     font-size: calc(
       15px + (27 - 15) * (${props => props.scale} - 320px) / (750 - 320)
     );
@@ -97,6 +144,13 @@ const MainHeading = styled.h2`
   @media (max-width: 750px) {
     grid-column: Img1-end / RightGutter-start;
     grid-row: MainHeading-start / MainHeading-end;
+    display: flex;
+    justify-content: center;
+    align-items: center;
+    font-size: calc(
+      30px + (50 - 30) * (${props => props.scale} - 320px) / (750 - 320)
+    );
+    font-weight: bold;
   }
 `;
 
@@ -105,14 +159,29 @@ const DescText = styled.p`
   border: 2px solid #8c4d47;
   /* ------------------------------------- */
   @media (max-width: 750px) {
+    width: calc(150px + (300 - 150) * (100vw - 320px) / (750 - 320));
     grid-column: LeftGutter-end / Img3-start;
     grid-row: Img2-end / Img3-end;
+    justify-self: center;
+
+    display: flex;
+    justify-content: center;
+    align-items: center;
+
+    text-align: center;
+    font-size: calc(
+      12px + (30 - 12) * (${props => props.scale} - 320px) / (750 - 320)
+    );
   }
 `;
 
 const Img1 = styled.img`
   /* ---------For Editing Layout---------- */
   border: 2px solid #488c6d;
+  width: 100%;
+  height: 100%;
+  position: relative;
+  z-index: 1;
   /* ------------------------------------- */
   object-fit: cover;
   /* width: calc(
@@ -129,6 +198,8 @@ const Img1 = styled.img`
 const Img2 = styled.img`
   /* ---------For Editing Layout---------- */
   border: 2px solid #8c8648;
+  width: 100%;
+  height: 100%;
   /* ------------------------------------- */
   object-fit: cover;
   /* width: calc(
@@ -145,6 +216,8 @@ const Img2 = styled.img`
 const Img3 = styled.img`
   /* ---------For Editing Layout---------- */
   border: 2px solid #47828c;
+  width: 100%;
+  height: 100%;
   /* ------------------------------------- */
   object-fit: cover;
   /* width: calc(
@@ -166,6 +239,15 @@ const Email = styled.h6`
   @media (max-width: 750px) {
     grid-column: LeftGutter-end / RightGutter-start;
     grid-row: Email-start / Email-end;
+
+    display: flex;
+    justify-content: center;
+    align-items: center;
+
+    font-size: calc(
+      15px + (50 - 15) * (${props => props.scale} - 320px) / (750 - 320)
+    );
+    font-weight: bold;
   }
 `;
 
@@ -173,9 +255,31 @@ const CTAButton = styled.button`
   /* ---------For Editing Layout---------- */
   border: 2px solid #8c4462;
   /* ------------------------------------- */
+  transition: 250ms;
+
+  &:hover {
+    background: #000;
+    color: white;
+  }
   @media (max-width: 750px) {
+    /* width: calc(120px + (130 - 100) * (100vw - 320px) / (750 - 320));
+    height: calc(50px + (60 - 50) * (100vh - 320px) / (750 - 320)); */
+    /* width: 25vw;
+    height: 6vh; */
     grid-column: LeftGutter-end / RightGutter-start;
     grid-row: Button-start / Button-end;
+    justify-self: center;
+    align-self: center;
+
+    display: flex;
+    justify-content: center;
+    align-items: center;
+    padding: 10px;
+
+    font-size: calc(
+      18px + (30 - 18) * (${props => props.scale} - 320px) / (750 - 320)
+    );
+    font-weight: bold;
   }
 `;
 
@@ -207,36 +311,41 @@ class APlayground extends Component {
   render() {
     return (
       <Wrapper>
-        <Logo scale={this.state.scale}>{/* Play Time */}</Logo>
+        <Logo scale={this.state.scale}>Play Time</Logo>
         <Nav scale={this.state.scale}>
-          {/* <li>Here</li>
-          <li>Comes</li>
-          <li>The</li>
-          <li>FunCooker</li> */}
+          <li>here</li>
+          <li>comes</li>
+          <li>the</li>
+          <li>funCooker</li>
         </Nav>
-        <MainHeading>{/* Ayo<br />River */}</MainHeading>
-        <DescText>
-          {/* Lorem ipsum dolor sit amet consectetur, adipisicing elit. Inventore in
+        <MainHeading scale={this.state.scale}>
+          Ayo<br />River
+        </MainHeading>
+        <DescText scale={this.state.scale}>
+          Lorem ipsum dolor sit amet consectetur, adipisicing elit. Inventore in
           blanditiis illum architecto eius! Nemo, sapiente! Perspiciatis id aut
-          velit labore molestias minima natus necessitatibus. */}
+          velit labore molestias minima natus necessitatibus.
         </DescText>
         <Img1
           scale={this.state.scale}
           // src="https://images.pexels.com/photos/776390/pexels-photo-776390.jpeg?auto=compress&cs=tinysrgb&h=650&w=940"
-          // alt="fun"
+          src="https://images.pexels.com/photos/127567/pexels-photo-127567.jpeg?auto=compress&cs=tinysrgb&dpr=2&h=750&w=1260"
+          alt="fun"
         />
         <Img2
           scale={this.state.scale}
           // src="https://c1.staticflickr.com/1/898/40555338765_8998d37c16_k.jpg"
-          // alt="fun"
+          src="https://images.pexels.com/photos/267858/pexels-photo-267858.jpeg?auto=compress&cs=tinysrgb&dpr=2&h=750&w=1260"
+          alt="fun"
         />
         <Img3
           scale={this.state.scale}
           // src="https://images.pexels.com/photos/922608/pexels-photo-922608.jpeg?auto=compress&cs=tinysrgb&dpr=2&h=350"
-          // alt="fun"
+          src="https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcQLpL8c3Mi1DjbjGLNxYvn7c_wpUq-mjUzmGbtSjlbdbASkvpZ64w"
+          alt="fun"
         />
-        <Email>{/* mraaroncasanova@gmail.com */}</Email>
-        <CTAButton>{/* get more */}</CTAButton>
+        <Email scale={this.state.scale}>mraaroncasanova@gmail.com</Email>
+        <CTAButton scale={this.state.scale}>get more</CTAButton>
       </Wrapper>
     );
   }
