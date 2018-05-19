71314c48ef1136467a84a6c915d896efc2044827
casyjs <1boris231@gmail.com>
2018-05-18 22:36:24 -0700

    add redirects page

--- src/ScaleContext.js
+++ src/ScaleContext.js
@@ -10,10 +10,8 @@ export class ScaleProvider extends Component {
   };
 
   getScale = () => {
-    // let scaleWidth = window.innerWidth;
-    // let scaleHeight = window.innerHeight;
-    let scaleWidth = window.outerWidth;
-    let scaleHeight = window.outerHeight;
+    let scaleWidth = window.innerWidth;
+    let scaleHeight = window.innerHeight;
     let scale = scaleWidth > scaleHeight ? scaleHeight : scaleWidth;
     return {
       scaleWidth,
