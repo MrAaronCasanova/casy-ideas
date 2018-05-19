b1ec1d4396985dc72fca1d0f775a5830e1b6ca4b
casyjs <1boris231@gmail.com>
2018-05-19 15:15:28 -0700

    bs commit revert

--- src/ScaleContext.js
+++ src/ScaleContext.js
@@ -10,10 +10,10 @@ export class ScaleProvider extends Component {
   };
   z;
   getScale = () => {
-    let scaleWidth = window.outerWidth;
-    let scaleHeight = window.outerHeight;
-    // let scaleWidth = window.innerWidth;
-    // let scaleHeight = window.innerHeight;
+    // let scaleWidth = window.outerWidth;
+    // let scaleHeight = window.outerHeight;
+    let scaleWidth = window.innerWidth;
+    let scaleHeight = window.innerHeight;
     let scale = scaleWidth > scaleHeight ? scaleHeight : scaleWidth;
     return {
       scaleWidth,
