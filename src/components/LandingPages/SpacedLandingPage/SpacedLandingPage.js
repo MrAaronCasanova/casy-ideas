928084ad26bbd6017814c6be284e02371a650918
casyjs <1boris231@gmail.com>
2018-05-21 10:25:17 -0700

    slightly refactored spaced landing to test vh issue

--- src/components/LandingPages/SpacedLandingPage/SpacedLandingPage.js
+++ src/components/LandingPages/SpacedLandingPage/SpacedLandingPage.js
@@ -1,60 +1,76 @@
 import React, { Component } from 'react';
 import styles from './SpacedLandingPage.css';
 import { ScaleContext } from './../../../ScaleContext';
+import { vpScale } from './../../../helpers';
+import styled from 'styled-components';
+
+const imageY = vpScale(100, 200, 320, 960, 'vhPx');
+
+const Image = styled.img`
+  --y: ${imageY}
+  width: 100%;
+  height: 100%;
+  object-fit: cover;
+  transform: translate(
+    calc(-50px + (-200 - -50) * (100vw - 320px) / (960 - 320)),
+    var(--y)
+  );
+  /* transform: translate(
+    calc(-50px + (-200 - -50) * (100vw - 320px) / (960 - 320)),
+    calc(100px + (200 - 100) * (100vh - 360px) / (960 - 360))
+  ); */
+`;
+
+const Circle = styled.div`
+  color: var(--circle-color);
+  border: 5px solid;
+  background: transparent;
+  position: absolute;
+  top: ${vpScale(19, 230, 320, 960, 'vhPx')};
+  left: calc(20px + (260 - 20) * (100vw - 320px) / (960 - 320));
+  width: ${props => vpScale(275, 500, 360, 960, 'sPx', props.scale)};
+  height: ${props => vpScale(275, 500, 360, 960, 'sPx', props.scale)};
+  border-radius: 50%;
+  overflow: hidden;
+  z-index: 1;
+`;
+
+const Heading = styled.h2`
+  font-size: ${props => vpScale(27, 40, 320, 960, 'sPx', props.scale)};
+`;
+
+const Text = styled.p`
+  font-size: ${props => vpScale(16, 32, 320, 960, 'sPx', props.scale)};
+`;
 
 class SpacedLandingPage extends Component {
-  circleSizeScale = scale => {
-    return 275 + (500 - 275) * (scale - 360) / (960 - 360);
-  };
-
-  circleHeadingScale = scale => {
-    return 27 + (40 - 27) * (scale - 320) / (960 - 320);
-  };
-
-  circleTextScale = scale => {
-    return 16 + (32 - 16) * (scale - 320) / (960 - 320);
-  };
-
   render() {
     return (
       <ScaleContext.Consumer>
         {context => (
           <div className={styles.SpacedLandingPage}>
-            <img
-              className={styles.image}
+            <Image
               src="https://images.pexels.com/photos/347761/pexels-photo-347761.jpeg?auto=compress&cs=tinysrgb&dpr=2&h=750&w=1260"
               alt="moon"
             />
-            <div
-              className={styles.circle}
-              style={{
-                width: `${this.circleSizeScale(context.state.scale)}px`,
-                height: `${this.circleSizeScale(context.state.scale)}px`
-              }}
-            >
+            <Circle scale={context.state.scale}>
               <div className={styles.boxOne}>
                 <div className={styles.circleContent}>
-                  <h2
+                  <Heading
+                    scale={context.state.scale}
                     className={styles.circleHeading}
-                    style={{
-                      fontSize: `${this.circleHeadingScale(
-                        context.state.scale
-                      )}px`
-                    }}
                   >
                     SPACED
-                  </h2>
-                  <p
+                  </Heading>
+                  <Text
+                    scale={context.state.scale}
                     className={styles.circleText}
-                    style={{
-                      fontSize: `${this.circleTextScale(context.state.scale)}px`
-                    }}
                   >
                     To space and back, safely
-                  </p>
+                  </Text>
                 </div>
               </div>
-            </div>
+            </Circle>
             <div className={styles.flipText}>
               <p>1st space airline company</p>
               <p>few steps to the moon</p>
